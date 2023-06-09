162d111e1164cdf75260d2671d74eee7d5a8354e
Matthias Hryniszak <padcom@gmail.com>
2023-06-09 22:29:23 +0200

    Code cleanup

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import plugin from 'tailwindcss/plugin'
 
 export default plugin(({ addComponents }) => {
-  // text-stroke-{inherited, none}
-  addComponents({
+  const components = {
     '.text-stroke-inherited': { '-webkit-text-stroke': 'inherited' },
     '.text-stroke-none': { '-webkit-text-stroke': '0' },
-  })
-
-  // text-stroke-[100..900]
-  const components = Array
-    .from(new Array(9), (x, index) => ({
-      [`.text-stroke-${(index + 1) * 100}`]: { '-webkit-text-stroke': `0.${((index + 1) * 2).toString().padStart(2, '0')}ex` }
-    }))
-    .reduce((acc, item) => ({ ...acc, ...item }), {})
+    '.text-stroke-100': { '-webkit-text-stroke': '0.02ex' },
+    '.text-stroke-200': { '-webkit-text-stroke': '0.04ex' },
+    '.text-stroke-300': { '-webkit-text-stroke': '0.06ex' },
+    '.text-stroke-400': { '-webkit-text-stroke': '0.08ex' },
+    '.text-stroke-500': { '-webkit-text-stroke': '0.10ex' },
+    '.text-stroke-600': { '-webkit-text-stroke': '0.12ex' },
+    '.text-stroke-700': { '-webkit-text-stroke': '0.14ex' },
+    '.text-stroke-800': { '-webkit-text-stroke': '0.16ex' },
+    '.text-stroke-900': { '-webkit-text-stroke': '0.18ex' },
+  }
 
   addComponents(components)
 })
